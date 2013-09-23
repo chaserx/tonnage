c1fa032a8e3fb220d370e972b3370e58c0b2d94e
Chase Southard <chase.southard@gmail.com>
2013-09-22 20:07:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tonnage::Application.config.secret_key_base = '3e8c18c043a71d6c11771605a68c364aebf432763db53e61b20f017af3d87034378d3e61162674d81cd718c08d9b3645cdd77ea996060ff088b5fc9bce4cc551'
