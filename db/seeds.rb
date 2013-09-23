d5600b67e417e701a7550f43d6d1e85c8154a3b8
Chase Southard <chase.southard@gmail.com>
2013-09-22 20:57:02 -0400

    adding some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'generating some weight measurements'
+dates = (1.month.ago.to_date..Date.today).map{ |date| date.strftime('%F') }
+30.times do
+  time = DateTime.parse(dates.sample)
+  WeightMeasurement.create({
+    value: Random.rand(208..240).round(2),
+    unit: 'lbs',
+    created_at: time,
+    updated_at: time
+  })
+end
